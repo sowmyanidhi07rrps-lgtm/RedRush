ff86df33aa3a147360d2f6416c8b66e54ee466ed
Shubhranshi Srivastava <shubhranshisrivastava@gmail.com>
2026-03-07 16:12:41 +0530

    Implemented table class

--- ConsoleUI.h
+++ ConsoleUI.h
@@ -8,19 +8,21 @@
 using namespace std;
 class Menu
 {
- private:
-   string title;
  public:
+   string title;
    Menu(string t)
    {
      title=t;
    }
 void display(string options[],int size)
 {
-  cout<<"\n===== "<< title << "====="<<endl;
-  for(int i=0;i<size;i++)
+  cout<<endl;
+  cout<<"===== "<< title << "====="<<endl;
+  int i=0;
+  while(i<size)
   {
     cout<<i+1<<". "<<options[i]<<endl;
+    i=i+1;
   }
   cout<<"Enter your choice: ";
   }
@@ -28,26 +30,71 @@ void display(string options[],int size)
 class Table
 {
   public:
-    static void showHeader()
+    string heading;
+    Table(string h)
     {
-      cout<<"\nName\tBlood Group\tCity\Contact"<<endl;
-      cout<<"---------------------------------------"<<endl;
+      heading=h;
     }
-static void showRow(string name,string blood,string city,string contact)
- {
-  cout<<name<<"\t"<<blood<<"\t\t"<<city<<"\t"<<contact<<endl;
- }
-};
-class Alert
-{
-  public:
-    static void success(string msg)
+    ~Table()
     {
-      cout<<"\nSuccess: "<<msg<<endl;
     }
-    static void error(string msg)
+    void printHeader(string col1,string col2,string col3)
     {
-      cout<,"\nError: "<<msg<<endl;
+      cout<<endl;
+      cout<<" "<< heading <<endl;
+      cout<<"+----------------+----------------+-----------------+"<<endl;
+      cout<<"|"<<col1;
+      int i=col1.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<col2;
+      i=col2.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<col3;
+      i=col3.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<endl;
+      cout<<"+-------------+---------------+--------------+"<<endl;
+    }
+    void printRow(string v1,string v2,string v3)
+    {
+      cout<<"|"<<v1;
+      int i= v1.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<v2;
+      i=v2.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<v3;
+      i=v3.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<endl;
+    }
+    void printFooter()
+    {
+      cout<<"+---------------+------------------+-----------------+"<<endl;
     }
 };
 #endif
@@ -56,7 +103,10 @@ int main()
   string options[]={"Add Donor","Search donor","Exit"};
   Menu m("Blood Bank System");
   m.display(options,3);
-Table::showHeader();
-Table::showRow("mahi","A+","Lucknow","9874521584");
-Alert::success("Donor Found");
-return 0;
+  Table t("Donor List");
+  t.printHeader("Name","Blood","City");
+  t.printRow("Ravi","A+","Delhi");
+  t.printRow("Aman","O+]","Lucknow");
+  t.printFooter();
+  return 0;
+}
