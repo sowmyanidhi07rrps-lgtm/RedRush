0f4be87802e4c225899a28ee71615253a0894f26
sowmyanidhi07rrps-lgtm <sowmyanidhi07rrps@gmail.com>
2026-03-09 22:30:39 +0530

    Implement blood matching logic in MatchingEngine

--- BloodSystem.cpp
+++ BloodSystem.cpp
@@ -32,6 +32,58 @@ int EmergencyRequest::operator>(EmergencyRequest r){
 		}
 		return 0;
 }
+void MatchingEngine::matchFromBank(EmergencyRequest req, BloodBank bank){
+	cout<<endl;
+	cout<<"==Searching Blood Ban==k"<<endl;
+	if(bank.isAvailable(req.bloodGroupNeeded)==1){
+		cout<<endl;
+		cout<<"====MATCH FOUND IN BLOOD GROUP===="<<endl;
+		cout<<"Blood Group: "<<req.bloodGroupNeeded<<endl;
+		cout<<"Patient Name: "<<req.patientName<<endl;
+	}
+	else{
+		cout<<"====MATCH NOT FOUND IN BLOOD BANK===="<<endl;
+	}
+}
+void MatchingEngine::matchFromDonors(EmergencyRequest req, Donor donors[], int donorCount){
+	cout<<endl;
+	cout<<"==Searching Donors=="<<endl;
+	int found=0;
+	int i=0;
+	while(i<donorCount){
+		if(donors[i].bloodGroup==req.bloodGroupNeeded && donors[i].isEligble()==1){
+			cout<<endl;
+			cout<<"==DONOR FOUND=="<<endl;
+			cout<<"Donor Name: "<<donors[i].name<<endl;
+			cout<<"Blood Group: "<<donors[i].bloodGroup<<endl;
+			cout<<"Patient Name: "<<req.patientName<<endl;
+			found=1;
+		}
+		i=i+1;
+	}
+	if(found==0){
+		cout<<"==NO ELIGIBLE DONOR FOUND=="<<endl;
+	}
+}
+void MatchingEngine::findMatch(EmergencyRequest req, bloodBank bank, Donor donors[], int donorCount){
+	cout<<endl;
+	cout<<"================================="<<endl;
+	cout<<"==START EMERGENCY MATCH=="<<endl;
+	cout<<"================================="<<endl;
+	req.display();
+	if(req.urgencyLevel=="CRITICAL"){
+		cout<<"!!!!!!!!!!!!!!!!!!!!!"<<endl;
+		cout<<"CRITICAL CASE URGENT"<<endl;
+		cout<<"!!!!!!!!!!!!!!!!!!!!!"<<endl;
+	}
+	if(bank.isAvailable(req.bloodGroupNeeded)==1){
+		matchFromBank(req,bank);
+	}
+	else{
+		matchFromBank(req,bank);
+		matchFromDonors(req,donors,donorCount);
+	}
+}
 int EmergencyRequest::showTotalRequests(){
 	return totalRequests;
 }
