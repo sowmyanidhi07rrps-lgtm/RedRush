0a16ef17a9fd463aaefd93f20a16212ed3b72b82
sowmyanidhi07rrps-lgtm <sowmyanidhi07rrps@gmail.com>
2026-03-09 19:36:27 +0530

    Define BloodBank, EmergencyRequest, and MatchingEngine classes

--- BloodSystem.h
+++ BloodSystem.h
@@ -11,3 +11,33 @@ template <class T>
 void ShowDetail(string label, T value){
 	cout<<label<<":"<<value<<endl;
 }
+class BloodBank{
+	BloodUnit units[100];
+	int count;
+	static int totalUnits;
+	Bloodbank();
+	~BloodBank();
+	void addUnit(BloodUnit u);
+	int isAvailable(string bloodGroup);
+	void displayInventory, checkExpiry();
+	static int showTotalUnits();
+	friend class MatchingEngine;
+};
+class EmergencyRequest{
+	public:
+		string patientName, bloodGroupNeeded, urgencyLevel;
+		static int totalRequests;
+		EmergencyRequest(string name, string bg, string urgency);
+		~EmergencyRequest();
+		void display();
+		void display(string straMessage);
+		int operator>(EmergencyRequest r);
+		static int showTotalRequests();
+};
+class MatchingEngine(){
+	public:
+			void MatchFromBank(EmergencyRequest req, BloodBank bank);
+			void MatchFromDonors(EmergencyRequest req, Donor donors[], int DonorCount);
+			void FindMatch(EmergencyRequest req, BloodBank bank, Donor donors[], int donorCount);
+};
+
