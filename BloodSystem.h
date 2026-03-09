13d9e4254d52a4969490850dc66b0db76ba2b41d
sowmyanidhi07rrps-lgtm <sowmyanidhi07rrps@gmail.com>
2026-03-09 13:29:34 +0530

    Add ShowDetail function template to BloodSystem.h

--- BloodSystem.h
+++ BloodSystem.h
@@ -3,3 +3,11 @@
 //Header File for blood coordination
 //Author:Sowmya
 //========================================
+
+#include<iostream>
+#include<string>
+#include<"Donor.h">
+template <class T>
+void ShowDetail(string label, T value){
+	cout<<label<<":"<<value<<endl;
+}
