0921ff2eeed239c3cfc271a8cc57aa1331289842
Shubhranshi Srivastava <shubhranshisrivastava@gmail.com>
2026-05-05 22:27:53 +0530

    added changes

--- ConsoleUI.cpp
+++ ConsoleUI.cpp
@@ -1,1 +1,88 @@
-
+//implementation
+#include "ConsoleUI.h"
+Menu::Menu(string t)
+{
+  title=t;
+}
+Menu::~Menu()
+{
+}
+void Menu::display(string options[],int size)
+{
+  cout<<endl;
+  cout<<"===== "<<title<< "====="<<endl;
+  int i=0;
+  while(i<size)
+  {
+    cout<<i+1<<". "<<options[i]<<endl;
+    i=i+1;
+  }
+  cout<<"Enter your choice: ";
+  }
+ Table::Table(string h)
+ {
+   heading=h;
+ }
+ Table::~Table()
+ {
+ }
+ void Table::printHeader(string col1,string col2,string col3)
+ {
+      cout<<endl;
+      cout<<" "<< heading <<endl;
+      cout<<"+----------------+----------------+-----------------+"<<endl;
+      cout<<"|"<<col1;
+      int i=col1.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<col2;
+      i=col2.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<col3;
+      i=col3.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<endl;
+      cout<<"+-------------+---------------+--------------+"<<endl;
+    }
+    void Table::printRow(string v1,string v2,string v3)
+    {
+      cout<<"|"<<v1;
+      int i= v1.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<v2;
+      i=v2.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<v3;
+      i=v3.length();
+      while(i<17)
+      {
+        cout<<" ";
+        i=i+1;
+      }
+      cout<<"|"<<endl;
+    }
+    void Table::printFooter()
+    {
+      cout<<"+-----------------+------------------+-----------------+"<<endl;
+    }
+  return 0;
+}
